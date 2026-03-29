b1b923512fe86450283b870a2abec6f1a469b60b
Dexight <tupikin@sfedu.ru>
2026-03-29 18:21:54 +0300

    bump mapping

--- 5 lab/script.js
+++ 5 lab/script.js
@@ -4,43 +4,33 @@ const gl = canvas.getContext("webgl2");
 // Класс для работы с объектами
 class GLObject 
 {
-    constructor(gl, program, objUrl, textureUrl, lighting, scale = [1.0, 1.0, 1.0]) 
+    constructor(gl, program, objUrl, textureUrl, bumpUrl, lighting, scale = [1.0, 1.0, 1.0]) 
     {
         this.gl = gl;
         this.program = program;
         this.objUrl = objUrl;
         this.textureUrl = textureUrl;
+        this.bumpUrl = bumpUrl;
         this.lighting = lighting;
         this.scale = scale;
         this.positionBuffer = null;
         this.texCoordBuffer = null;
-        this.normalBuffer = null; // Добавляем буфер для нормалей
+        this.normalBuffer = null;  // Добавляем буфер для нормалей
+        this.tangentBuffer = null; // Буфер для касательных (для bump mapping)
         this.texture = null;
+        this.bumpMap = null;
         this.offsets = new Float32Array([0.0, 0.0, 0.0]);
         this.objData = null;
         this.programPhong = program;
-    }
-
-    async changeLightingModel() 
-    {
-        if (this.lighting === "phong") 
-        {
-            this.program = this.programPhong;
-        } 
-        else if (this.lighting === "toonshading") 
-        {
-            this.program = this.programToon;
-        }
-        else if (this.lighting === "other")
-        {
-            this.program = this.programOther;
-        }
     }
 
     async init() 
     {
         // Загрузка OBJ-файла
         this.objData = await this.loadOBJ(this.objUrl);
+
+        // BUMP
+        this.computeTangents();
 
         // Создание и привязка буферов
         this.positionBuffer = this.gl.createBuffer();
@@ -54,6 +44,10 @@ class GLObject
         this.normalBuffer = this.gl.createBuffer(); // Создаем буфер для нормалей
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.normalBuffer);
         this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.objData.normals), this.gl.STATIC_DRAW);
+
+        this.tangentBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.tangentBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.objData.tangents), this.gl.STATIC_DRAW);
 
         // Создание текстуры
         this.texture = this.gl.createTexture();
@@ -69,6 +63,24 @@ class GLObject
             image.onload = () => {
                 this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
                 this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, image);
+                this.gl.generateMipmap(this.gl.TEXTURE_2D);
+                resolve();
+            };
+        });
+
+        this.bumpMap = this.gl.createTexture();
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.bumpMap);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+
+        const bumpImage = new Image();
+        bumpImage.src = this.bumpUrl;
+        await new Promise((resolve) => {
+            bumpImage.onload = () => {
+                this.gl.bindTexture(this.gl.TEXTURE_2D, this.bumpMap);
+                this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, bumpImage);
                 this.gl.generateMipmap(this.gl.TEXTURE_2D);
                 resolve();
             };
@@ -133,9 +145,78 @@ class GLObject
         };
     }
 
+    // Вычисление касательных для каждой вершины (для bump mapping)
+    computeTangents() {
+        const positions = this.objData.positions;
+        const texCoords = this.objData.texCoords;
+        const normals = this.objData.normals;
+        
+        if (positions.length === 0 || texCoords.length === 0) {
+            return;
+        }
+
+        const vertexCount = positions.length / 3;
+        const tangents = new Array(vertexCount);
+        for (let i = 0; i < vertexCount; i++) {
+            tangents[i] = [0, 0, 0];
+        }
+
+        // Проходим по треугольникам (каждые 3 вершины образуют треугольник)
+        for (let i = 0; i < positions.length; i += 9) {
+            // Вершины треугольника
+            const v1 = [positions[i], positions[i+1], positions[i+2]];
+            const v2 = [positions[i+3], positions[i+4], positions[i+5]];
+            const v3 = [positions[i+6], positions[i+7], positions[i+8]];
+            
+            // Текстурные координаты
+            const uv1 = [texCoords[i/3 * 2], texCoords[i/3 * 2 + 1]];
+            const uv2 = [texCoords[(i/3 + 1) * 2], texCoords[(i/3 + 1) * 2 + 1]];
+            const uv3 = [texCoords[(i/3 + 2) * 2], texCoords[(i/3 + 2) * 2 + 1]];
+            
+            // Вычисление векторов ребер
+            const edge1 = [v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]];
+            const edge2 = [v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2]];
+            
+            // Вычисление дельт текстурных координат
+            const deltaUV1 = [uv2[0] - uv1[0], uv2[1] - uv1[1]];
+            const deltaUV2 = [uv3[0] - uv1[0], uv3[1] - uv1[1]];
+            
+            // Вычисление касательной
+            const f = 1.0 / (deltaUV1[0] * deltaUV2[1] - deltaUV2[0] * deltaUV1[1]);
+            
+            const tangent = [
+                f * (deltaUV2[1] * edge1[0] - deltaUV1[1] * edge2[0]),
+                f * (deltaUV2[1] * edge1[1] - deltaUV1[1] * edge2[1]),
+                f * (deltaUV2[1] * edge1[2] - deltaUV1[1] * edge2[2])
+            ];
+            
+            // Добавляем касательную к каждой вершине треугольника
+            for (let j = 0; j < 3; j++) {
+                const idx = i/3 + j;
+                tangents[idx][0] += tangent[0];
+                tangents[idx][1] += tangent[1];
+                tangents[idx][2] += tangent[2];
+            }
+        }
+        
+        // Нормализация касательных
+        const finalTangents = [];
+        for (let i = 0; i < vertexCount; i++) {
+            const t = tangents[i];
+            const len = Math.sqrt(t[0]*t[0] + t[1]*t[1] + t[2]*t[2]);
+            if (len > 0) {
+                finalTangents.push(t[0]/len, t[1]/len, t[2]/len);
+            } else {
+                finalTangents.push(1, 0, 0);
+            }
+        }
+        
+        this.objData.tangents = finalTangents;
+    }
+
     setOffsets(offsets) { this.offsets = new Float32Array(offsets); }
 
-    render(modelMatrix, mvpMatrix, aPosition, aTexCoord, aOffsetLocation, uTextureLocation, uModelMatrix, uMVPMatrix, cameraPosition, cameraTarget, cameraUp) 
+    render(modelMatrix, mvpMatrix, aPosition, aTexCoord, aOffsetLocation, uTextureLocation, uModelMatrix, uMVPMatrix, cameraPosition, cameraTarget, cameraUp, uBumpMapLocation, uBumpStrength, bumpStrengthValue) 
     {
         updateModelViewMatrix(modelMatrix, cameraPosition, cameraTarget, cameraUp);
 
@@ -147,10 +228,15 @@ class GLObject
         this.gl.vertexAttribPointer(aTexCoord, 2, this.gl.FLOAT, false, 0, 0);
         this.gl.enableVertexAttribArray(aTexCoord);
 
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.normalBuffer); // Привязываем буфер нормалей
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.normalBuffer); 
         const aNormal = this.gl.getAttribLocation(this.program, "aNormal");
         this.gl.vertexAttribPointer(aNormal, 3, this.gl.FLOAT, false, 0, 0);
         this.gl.enableVertexAttribArray(aNormal);
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.tangentBuffer); // Привязываем BUMP буфер
+        const aTangent = this.gl.getAttribLocation(this.program, "aTangent");
+        this.gl.vertexAttribPointer(aTangent, 3, this.gl.FLOAT, false, 0, 0);
+        this.gl.enableVertexAttribArray(aTangent);
 
         const offsetBuffer = this.gl.createBuffer();
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, offsetBuffer);
@@ -163,6 +249,12 @@ class GLObject
         this.gl.activeTexture(this.gl.TEXTURE0);
         this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
         this.gl.uniform1i(uTextureLocation, 0);
+
+        // Bump map
+        this.gl.activeTexture(this.gl.TEXTURE1);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.bumpMap);
+        this.gl.uniform1i(uBumpMapLocation, 1);
+        this.gl.uniform1f(uBumpStrength, bumpStrengthValue);
 
         mat4.scale(modelMatrix, modelMatrix, this.scale);
         this.gl.uniformMatrix4fv(uModelMatrix, false, modelMatrix);
@@ -180,6 +272,7 @@ in vec3 aPosition;
 in vec2 aTexCoord;
 in vec3 aOffset;
 in vec3 aNormal;
+in vec3 aTangent;
 
 uniform mat4 uMVPMatrix;
 uniform mat4 uModelMatrix;
@@ -187,13 +280,15 @@ uniform mat4 uModelMatrix;
 out vec2 vTexCoord;
 out vec3 vPosition;
 out vec3 vNormal;
+out vec3 vTangent;
 
 void main() {
     vec4 worldPosition = uModelMatrix * vec4(aPosition, 1.0) + vec4(aOffset, 0.0);
     gl_Position = uMVPMatrix * worldPosition;
-    vTexCoord = vec2(aTexCoord.x, 1.0 - aTexCoord.y); // Invert Y coordinate
+    vTexCoord = vec2(aTexCoord.x, 1.0 - aTexCoord.y);
     vPosition = worldPosition.xyz;
-    vNormal = mat3(uModelMatrix) * aNormal; // Transform normal to world space
+    vNormal = mat3(uModelMatrix) * aNormal;
+    vTangent = mat3(uModelMatrix) * aTangent;
 }
 `;
 
@@ -203,21 +298,47 @@ precision highp float;
 in vec2 vTexCoord;
 in vec3 vPosition;
 in vec3 vNormal;
+in vec3 vTangent;
 
 uniform sampler2D uTexture;
+uniform sampler2D uBumpMap;
+uniform float uBumpStrength;
 
 uniform vec3 uViewPosition;
-
 uniform vec3 uPointLightPosition;
 uniform vec3 uPointLightColor;
 
 out vec4 fragColor;
 
+// Функция для получения возмущенной нормали из bump map
+vec3 getBumpedNormal() {
+    float height = texture(uBumpMap, vTexCoord).r;// Высота
+    
+    // Градиент высоты по текстурным координатам
+    float offset = 1.0 / 512.0;
+    float h1 = texture(uBumpMap, vTexCoord + vec2(offset, 0.0)).r;
+    float h2 = texture(uBumpMap, vTexCoord + vec2(0.0, offset)).r;
+    
+    float du = (h1 - height) * uBumpStrength;
+    float dv = (h2 - height) * uBumpStrength;
+    
+    // TBN матрица
+    vec3 N = normalize(vNormal);
+    vec3 T = normalize(vTangent);
+    vec3 B = cross(N, T);
+    
+    // Вектор возмущения
+    vec3 perturbedNormalTS = normalize(vec3(-du, -dv, 1.0));
+    
+    // Возмущенная нормаль в мировое пространство
+    return normalize(T * perturbedNormalTS.x + B * perturbedNormalTS.y + N * perturbedNormalTS.z);
+}
+
 void main() {
     vec4 texColor = texture(uTexture, vTexCoord);
 
-    // Нормализация нормали
-    vec3 normal = normalize(vNormal);
+    // Возмущенная нормаль из bump map
+    vec3 normal = getBumpedNormal();
 
     // Ambient light
     vec3 ambient = 0.1 * texColor.rgb;
@@ -227,8 +348,8 @@ void main() {
     vec3 viewDir = normalize(uViewPosition - vPosition);
     vec3 reflectDirPoint = reflect(-pointLightDir, normal);
 
-    float pointSpecularStrength = 0.5; // Интенсивность зеркального отражения
-    float specPoint = pow(max(dot(viewDir, reflectDirPoint), 0.0), 16.0); // Зеркальный бликовый эффект
+    float pointSpecularStrength = 0.5;
+    float specPoint = pow(max(dot(viewDir, reflectDirPoint), 0.0), 16.0);
     vec3 pointLightSpecular = pointSpecularStrength * specPoint * uPointLightColor;
 
     float pointLightDistance = length(uPointLightPosition - vPosition);
@@ -285,6 +406,8 @@ let uModelMatrix
 let uPointLightPosition
 let uPointLightColor
 let uViewPosition
+let uBumpMapLocation
+let uBumpStrength
 
 function changeLocations(gl, program)
 {
@@ -304,6 +427,10 @@ function changeLocations(gl, program)
 
     // Камера
     uViewPosition = gl.getUniformLocation(program, "uViewPosition");
+
+    //Bump
+    uBumpMapLocation = gl.getUniformLocation(program, "uBumpMap");
+    uBumpStrength = gl.getUniformLocation(program, "uBumpStrength");
 }
 
 function updateModelViewMatrix(modelMatrix, cameraPosition, cameraTarget, cameraUp) 
@@ -318,7 +445,7 @@ function updateModelViewMatrix(modelMatrix, cameraPosition, cameraTarget, camera
     const program = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
     // Создание объектов
-    const sphere = new GLObject(gl, program, "sphere.obj","color.jpg", "phong", [20.0, 20.0, 20.0] )
+    const sphere = new GLObject(gl, program, "sphere.obj","color.jpg", "bump.jpg", "phong", [20.0, 20.0, 20.0] )
     // Инициализация объектов
     await sphere.init();
     //Установка смещений для sphere
@@ -345,6 +472,12 @@ function updateModelViewMatrix(modelMatrix, cameraPosition, cameraTarget, camera
         pointLightPosition[2] = parseFloat(event.target.value);
     });
 
+    // Сила bump'а
+    let bumpStrength = 0.1;
+    document.getElementById('bumpStrength').addEventListener('input', (event) => {
+        bumpStrength = parseFloat(event.target.value);
+    });
+
     // Рендеринг
     function render() 
     {
@@ -362,7 +495,7 @@ function updateModelViewMatrix(modelMatrix, cameraPosition, cameraTarget, camera
         //отрисовка sphere
         changeLocations(sphere.gl, sphere.program);
         setParameters(sphere.gl);
-        sphere.render(modelMatrix, mvpMatrix, aPosition, aTexCoord, aOffsetLocation, uTextureLocation, uModelMatrix, uMVPMatrix, cameraPosition, cameraTarget, cameraUp)
+        sphere.render(modelMatrix, mvpMatrix, aPosition, aTexCoord, aOffsetLocation, uTextureLocation, uModelMatrix, uMVPMatrix, cameraPosition, cameraTarget, cameraUp, uBumpMapLocation, uBumpStrength, bumpStrength)
         
         requestAnimationFrame(render);
     }
